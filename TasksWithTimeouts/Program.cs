2fb87dca9febb9e3fa40901b8f77829c7aa27553
VitaliyH <vitalii.harbovskyi@devoxsoftware.com>
2019-06-14 16:47:51 +0300

    first commit

---
+++ TasksWithTimeouts/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TasksWithTimeouts
+{
+    public class Program
+    {
+        
+
+        public static async Task Main(string[] args)
+        {
+            var rotator = new ProxyRotatorService();
+            rotator.ChangeProxy();
+            while (true)
+            {
+   
+                try
+                {
+                    await rotator.WebAccessViaProxyRotatorApi("https://www.expressvpn.com/ru/what-is-my-ip");
+                    Thread.Sleep(1000);
+                    rotator.ChangeProxy();
+                }
+                catch (Exception io)
+                {
+                    Console.WriteLine("Exception:"+io.Message);
+                    
+                    rotator.ChangeProxy();
+     
+                    Thread.Sleep(1000);
+                } 
+            }
+     
+
+        }
+    }
+}
---
+++ TasksWithTimeouts/ProxyRotatorService.cs
@@ -0,0 +1,84 @@
+using System;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Xml;
+using Newtonsoft.Json;
+
+namespace TasksWithTimeouts
+{
+    public class ProxyRotatorService
+    {
+        
+        
+        private void ChangeProxy()
+        {
+            WebRequest.DefaultWebProxy = new WebProxy();
+            string serviceUri =
+                "http://falcon.proxyrotator.com:51337/?apiKey=&get=true";
+            string jsonString = string.Empty;
+            string proxyAddress = string.Empty;
+
+            HttpWebRequest request = (HttpWebRequest) WebRequest.Create(serviceUri);
+
+            using (HttpWebResponse response = (HttpWebResponse) request.GetResponse())
+            using (Stream stream = response.GetResponseStream())
+            using (StreamReader reader = new StreamReader(stream ?? throw new InvalidOperationException()))
+            {
+                jsonString = reader.ReadToEnd();
+                var px = JsonConvert.DeserializeObject<prxy>(jsonString);
+                proxyAddress = "http://" + ((prxy) px).proxy + "/";
+            }
+            WebProxy proxyObject = new WebProxy(proxyAddress);
+            WebRequest.DefaultWebProxy = proxyObject;
+
+        }
+        public async Task WebAccessViaProxyRotatorApi(string url)
+        {
+   
+            // create a request
+            HttpWebRequest request = (HttpWebRequest) WebRequest.Create(url);
+
+            using (HttpWebResponse response = (HttpWebResponse) request.GetResponse())
+            using (Stream stream = response.GetResponseStream())
+            using (StreamReader reader = new StreamReader(stream ?? throw new InvalidOperationException()))
+            {
+                var jsonString = reader.ReadToEnd();
+                Console.WriteLine(getIpAddress(jsonString));
+            }
+        }
+
+        private string getIpAddress(string source)
+        {
+            var sPattern =  new Regex(@"<p class='ip-address'>\n?(.*)\n?</p>");
+            var result =  sPattern.Match(source);
+            if (result.Groups.Count > 0)
+            {
+                return result.Groups[1].Value;
+            }
+            return "ip address not found";
+        }
+    }
+
+    public class prxy
+    {
+        public string proxy { get; set; }
+        public string ip { get; set; }
+        public string port { get; set; }
+        public string type { get; set; }
+        public int lastChecked { get; set; }
+        public bool get { get; set; }
+        public bool post { get; set; }
+        public bool cookies { get; set; }
+        public bool referer { get; set; }
+        public bool userAgent { get; set; }
+        public string city { get; set; }
+        public string state { get; set; }
+        public string country { get; set; }
+        public int currentThreads { get; set; }
+        public int threadsAllowed { get; set; }
+    }
+}
